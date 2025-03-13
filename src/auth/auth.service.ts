b086902712bafbd2e9d85bd120612f0b7e0578c1
Masihbalali <Masihbalali@outlook.com>
2025-03-14 01:47:18 +0330

    prisma migrate, and create user API.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@nestjs/common';
-
+import { AuthDto } from './dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from "argon2"
 @Injectable()
 export class AuthService {
+    constructor(private prisma: PrismaService) { }
+
+    async signup(dto: AuthDto) {
+        // generate the password hash
+        const hash = await argon.hash(dto.password)
+
+        // saave the new user in the DB
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                userName: dto.username,
+                hash
+            }
+        })
+
+        // we can return data without hash
+        delete user.hash
+
+        // return the saved user
+        return user
+    }
 
     signin() {
         return "sign in from auth service"
-    }
-
-    signup() {
-        return "sign UP from auth service"
     }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({
+    // just allow the item that we need(declare in DTO) to came from API
+    whitelist: true
+  }))
   await app.listen(3000);
 }
 bootstrap();
