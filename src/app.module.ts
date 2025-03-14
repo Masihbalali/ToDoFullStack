68976df794686549d1e12359688ca2d0037437e7
Masihbalali <Masihbalali@outlook.com>
2025-03-14 13:37:10 +0330

    nest config setup.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ UsersModule, PrismaModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    UsersModule,
+    PrismaModule,
+    AuthModule],
+
 })
-export class AppModule {}
+export class AppModule { }
