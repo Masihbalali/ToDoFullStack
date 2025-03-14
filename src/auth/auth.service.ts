58e53d7fb6d1bc496b0c8f74b94f68aa539aa0b2
Masihbalali <Masihbalali@outlook.com>
2025-03-14 15:07:55 +0330

    feat: jwt setup and signtoken function to create jwt token.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,15 @@ import { AuthDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from "argon2"
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService
+    ) { }
 
     async signup(dto: AuthDto) {
         // generate the password hash
@@ -27,7 +33,7 @@ export class AuthService {
             delete user.hash
 
             // return the saved user
-            return user
+            return this.signToken(user.id, user.email)
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
                 if (error.code === 'P2002') {
@@ -71,7 +77,26 @@ export class AuthService {
         delete user.hash
 
         // send back the user
-        return user
+        return this.signToken(user.id, user.email)
+    }
+
+
+    async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+        const paylaod = {
+            sub: userId,
+            email
+        }
+        const secret = this.config.get('JWT_SECRET')
+
+        const token = await this.jwt.signAsync(paylaod, {
+            expiresIn: '15min',
+            secret: secret
+        })
+
+        return {
+            access_token: token
+        }
+
     }
 
 }
